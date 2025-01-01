2f04c8a166bd3116e9e9c3d2af09962a330bb8c3
Abdallah Awwad <ar.awwad6@gmail.com>
2025-01-01 16:38:58 +0100

    enhance crud operations * add parital updates to movies * edit conflict control * timeout long queries with context

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"context"
 	"database/sql"
 	"errors"
 	"time"
@@ -49,9 +50,11 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
     FROM movies
     WHERE id = $1
   `
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
 
 	var movie Movie
-	err := m.DB.QueryRow(query, id).Scan(
+	err := m.DB.QueryRowContext(ctx, query, id).Scan(
 		&movie.ID,
 		&movie.CreatedAt,
 		&movie.Title,
@@ -88,13 +91,25 @@ func (m MovieModel) Update(movie *Movie) error {
 	query := `
     UPDATE movies
     SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
-    WHERE id = $5
+    WHERE id = $5 AND version = $6
     RETURNING version;
   `
 
-	args := []any{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres), movie.ID}
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
 
-	return m.DB.QueryRow(query, args...).Scan(&movie.Version)
+	args := []any{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres), movie.ID, movie.Version}
+
+	err := m.DB.QueryRowContext(ctx, query, args...).Scan(&movie.Version)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return ErrEditConflict
+		default:
+			return err
+		}
+	}
+	return nil
 }
 
 func (m MovieModel) Delete(id int64) error {
@@ -106,8 +121,10 @@ func (m MovieModel) Delete(id int64) error {
     DELETE FROM movies
     WHERE id = $1;
   `
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
 
-	result, err := m.DB.Exec(query, id)
+	result, err := m.DB.ExecContext(ctx, query, id)
 	if err != nil {
 		return err
 	}
